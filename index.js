0e856811430ca27fdbb091bf07e4005aaf3843de
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-03-23 16:42:37 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 =31;
+let num2= 2;
+let num3=19;
+let num4=5;
+
+let multiply = num1 * num2;
+
+let random=Math.ceil(Math.random());
+
+let mod = num3%num4;
+
+let max = Math.max(12,6,3,20);
